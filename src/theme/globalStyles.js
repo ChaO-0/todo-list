cc0d29bc0313e5431c3cafa55796c8ca31585e21
Christopher Yu <christophergin88@gmail.com>
2021-06-24 01:41:41 +0800

    feat: update global style

--- src/theme/globalStyles.js
+++ src/theme/globalStyles.js
@@ -4,5 +4,9 @@ const GlobalStyle = createGlobalStyle`
   body {
     background: hsl(236, 33%, 92%)
   }
+
+  .list-item:hover .remove-icon{
+    display: block
+  }
 `;
 export default GlobalStyle;
