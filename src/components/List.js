5f7963f31fa442cc5869e579863a8299294b5c31
Christopher Yu <christophergin88@gmail.com>
2021-06-25 01:49:02 +0800

    feat: add filter feature

--- src/components/List.js
+++ src/components/List.js
@@ -1,26 +1,87 @@
+import { useState } from 'react';
 import ListItem from './ListItem';
 
 const List = ({ items, setItems }) => {
   const handleDelete = (elm) => {
-    setItems((prev) => prev.filter((item) => item !== elm));
+    setItems((prev) => prev.filter((item) => item.id !== elm));
   };
+
+  const activeItems = items.filter((item) => !item.completed);
+
+  const completedItems = items.filter((item) => item.completed);
+
+  const handleClick = (type) => {
+    setFilterType(type);
+    if (type === 'active') {
+    }
+  };
+
+  const [filterType, setFilterType] = useState('all');
 
   return (
     <>
       <ul className="mt-5 rounded-t-md bg-white text-light cursor-pointer">
-        {items.map((item, idx) => (
-          <ListItem item={item} key={idx} handleDelete={handleDelete} />
-        ))}
+        {filterType === 'all' &&
+          items.map((item, idx) => (
+            <ListItem
+              item={item}
+              setItems={setItems}
+              key={idx}
+              handleDelete={handleDelete}
+            />
+          ))}
+
+        {filterType === 'active' &&
+          activeItems.map((item, idx) => (
+            <ListItem
+              item={item}
+              setItems={setItems}
+              key={idx}
+              handleDelete={handleDelete}
+            />
+          ))}
+
+        {filterType === 'completed' &&
+          completedItems.map((item, idx) => (
+            <ListItem
+              item={item}
+              setItems={setItems}
+              key={idx}
+              handleDelete={handleDelete}
+            />
+          ))}
       </ul>
 
-      <div className="flex justify-between text-light text-xs px-6 py-4 bg-white rounded-b-md">
+      <div className="flex justify-between text-lightGray text-xs px-6 py-4 bg-white rounded-b-md">
         <div>{items.length} items left</div>
         <div className="flex justify-between gap-7">
-          <div className="cursor-pointer">All</div>
-          <div className="cursor-pointer">Active</div>
-          <div className="cursor-pointer">Completed</div>
+          <div
+            className={`cursor-pointer hover:text-light ${
+              filterType === 'all' && 'text-blueFilter hover:text-blueFilter'
+            }`}
+            onClick={() => handleClick('all')}
+          >
+            All
+          </div>
+          <div
+            className={`cursor-pointer hover:text-light ${
+              filterType === 'active' && 'text-blueFilter hover:text-blueFilter'
+            }`}
+            onClick={() => handleClick('active')}
+          >
+            Active
+          </div>
+          <div
+            className={`cursor-pointer hover:text-light ${
+              filterType === 'completed' &&
+              'text-blueFilter hover:text-blueFilter'
+            }`}
+            onClick={() => handleClick('completed')}
+          >
+            Completed
+          </div>
         </div>
-        <div className="cursor-pointer">Clear completed</div>
+        <div className="cursor-pointer hover:text-light">Clear completed</div>
       </div>
     </>
   );
