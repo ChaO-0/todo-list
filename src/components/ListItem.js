20c008c6a0291c9d6185e14965349e064bbf636a
Christopher Yu <christophergin88@gmail.com>
2021-06-25 01:49:23 +0800

    refactor: check item

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,19 +1,26 @@
-import { useState } from 'react';
 import checkIcon from '../images/icon-check.svg';
 import crossIcon from '../images/icon-cross.svg';
 
-const ListItem = ({ item, handleDelete }) => {
-  const [check, setCheck] = useState(false);
-
+const ListItem = ({ item, setItems, handleDelete }) => {
   const handleClick = () => {
-    setCheck((prev) => !prev);
+    setItems((prev) => {
+      const elementsIndex = prev.findIndex((elm) => elm.id === item.id);
+      let newArray = [...prev];
+      newArray[elementsIndex] = {
+        ...newArray[elementsIndex],
+        completed: !newArray[elementsIndex].completed,
+      };
+      return newArray;
+    });
   };
 
   return (
     <li className="border-b-2 py-4 relative list-item">
       <span
         className={`absolute border-2 rounded-full w-6 h-6 bottom-5 left-5 cursor-pointer ${
-          check ? 'bg-gradient-to-br from-blueCheck to-purpleCheck' : 'bg-white'
+          item.completed
+            ? 'bg-gradient-to-br from-blueCheck to-purpleCheck'
+            : 'bg-white'
         } `}
         onClick={handleClick}
       >
@@ -23,12 +30,14 @@ const ListItem = ({ item, handleDelete }) => {
           className="object-none object-center w-5 h-5"
         />
       </span>
-      <div className={`pl-14 ${check && 'line-through text-completed'}`}>
-        {item}
+      <div
+        className={`pl-14 ${item.completed && 'line-through text-completed'}`}
+      >
+        {item.text}
       </div>
       <span
         className="absolute cursor-pointer w-6 h-6 bottom-3.5 right-5 hidden remove-icon"
-        onClick={() => handleDelete(item)}
+        onClick={() => handleDelete(item.id)}
       >
         <img
           src={crossIcon}
